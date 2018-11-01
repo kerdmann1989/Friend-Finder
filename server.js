386a75886e3c5e8973dd3a979799847dcab132e4
Kathy Erdmann <kathryn.erdmann@hsc.utah.edu>
2018-10-31 19:36:28 -0600

    added links to API and Github

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var express = require("express");
 
-
 var app = express();
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({  extended: true }));;
 app.use(express.json());
